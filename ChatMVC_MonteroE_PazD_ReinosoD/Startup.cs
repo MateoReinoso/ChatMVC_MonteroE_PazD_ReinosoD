a5440afc6fedd6d420a0eed00ef90f5e2be91746
Mateo <mateo_riva@hotmail.com>
2021-01-03 19:09:51 -0500

    Chat MVC

---
+++ ChatMVC_MonteroE_PazD_ReinosoD/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(ChatMVC_MonteroE_PazD_ReinosoD.Startup))]
+
+namespace ChatMVC_MonteroE_PazD_ReinosoD
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obtener más información sobre cómo configurar la aplicación, visite https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
